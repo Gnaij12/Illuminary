606c7256317524908065527d5415e5015d35f01c
Alan Jiang <alanzjiang10@gmail.com>
2024-03-24 09:10:42 -0400

    Backend updated

--- Sentiment_Analysis/training2.py
+++ Sentiment_Analysis/training2.py
@@ -1,15 +1,25 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from nltk.corpus import stopwords
 from textblob import TextBlob
 from textblob import Word
 import nltk
 from nltk.corpus import stopwords
 from textblob import Word
-from nltk.tokenize import word_tokenize
+# from nltk.tokenize import word_tokenize
 import string
+
+# from sklearn.tree import DecisionTreeRegressor
+# from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeClassifier
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.naive_bayes import MultinomialNB
+ 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+# from sklearn.metrics import accuracy_score
+# from sklearn.metrics import confusion_matrix
+# from sklearn.metrics import classification_report
 
 def process_string(s):
     s = " ".join(x.lower() for x in s.split())
@@ -21,114 +31,106 @@ def process_string(s):
     s = TextBlob(s).words
     return s
 
-import os
-for dirname, _, filenames in os.walk('/kaggle/input'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
-        
-df = pd.read_csv('./Sentiment_Analysis/Mental-Health-Twitter-Modified.csv')
+def make_model():
 
-# Change all characters in tweets to lower case
-df["post_text"] = df["post_text"].apply(lambda x: " ".join(x.lower() for x in x.split()))
+    import os
+    for dirname, _, filenames in os.walk('/kaggle/input'):
+        for filename in filenames:
+            print(os.path.join(dirname, filename))
+            
+    df = pd.read_csv('./Sentiment_Analysis/Mental-Health-Twitter-Modified.csv')
 
-# Remove numbers from tweets
-df["post_text"] = df["post_text"].str.replace("\d","")
+    # Change all characters in tweets to lower case
+    df["post_text"] = df["post_text"].apply(lambda x: " ".join(x.lower() for x in x.split()))
 
-# Remove punctuation from tweets
-df["post_text"] = df["post_text"].str.replace("[^\w\s]","")
+    # Remove numbers from tweets
+    df["post_text"] = df["post_text"].str.replace("\d","")
 
-# Remove stop words
-# nltk.download("stopwords")
-sw = stopwords.words("english")
-df["post_text"] = df["post_text"].apply(lambda x: " ".join(x for x in x.split() if x not in sw))
+    # Remove punctuation from tweets
+    df["post_text"] = df["post_text"].str.replace("[^\w\s]","")
 
-# nltk.download("wordnet")
-# nltk.download("omw-1.4")
-df["post_text"] = df["post_text"].apply(lambda x: " ".join([Word(x).lemmatize()]))
+    # Remove stop words
+    # nltk.download("stopwords")
+    sw = stopwords.words("english")
+    df["post_text"] = df["post_text"].apply(lambda x: " ".join(x for x in x.split() if x not in sw))
 
-# nltk.download('punkt')
-df["tokens"] = df["post_text"].apply(lambda x: TextBlob(x).words)
+    # nltk.download("wordnet")
+    # nltk.download("omw-1.4")
+    df["post_text"] = df["post_text"].apply(lambda x: " ".join([Word(x).lemmatize()]))
 
-# Applying sentiment to entire dataset
+    # nltk.download('punkt')
+    df["tokens"] = df["post_text"].apply(lambda x: TextBlob(x).words)
 
-blob_emptylist = []
+    # Applying sentiment to entire dataset
 
-for i in df["post_text"]:
-    blob = TextBlob(i).sentiment # returns polarity
-    blob_emptylist.append(blob)
+    blob_emptylist = []
 
-# Create a new dataframe to show polarity and subjectivity for each tweet
-df2 = pd.DataFrame(blob_emptylist)
+    for i in df["post_text"]:
+        blob = TextBlob(i).sentiment # returns polarity
+        blob_emptylist.append(blob)
 
-df3 = pd.concat([df.reset_index(drop=True), df2], axis=1)
-df4 = df3[['post_text','tokens','polarity','severity','severity_int']]
-# print(df4["polarity"])
-# print(df4["severity_int"])
-# df4["Sentiment"] =  np.where(df4["polarity"] >= 0 , "Positive", "Negative")
-df4["10Polarity"] = df4["polarity"].round(1) * 10
-df4["roundedPolarity"] = df4['polarity'].round(1)
+    # Create a new dataframe to show polarity and subjectivity for each tweet
+    df2 = pd.DataFrame(blob_emptylist)
 
-# print(df4["polarity"])
-# print(df4["10polarity"])
-# print(df4["roundedPolarity"])
+    df3 = pd.concat([df.reset_index(drop=True), df2], axis=1)
+    df4 = df3[['post_text','tokens','polarity','severity','severity_int']]
+    # print(df4["polarity"])
+    # print(df4["severity_int"])
+    # df4["Sentiment"] =  np.where(df4["polarity"] >= 0 , "Positive", "Negative")
+    df4["10Polarity"] = df4["polarity"].round(1) * 10
+    df4["roundedPolarity"] = df4['polarity'].round(1)
 
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import MultinomialNB
- 
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
+    # print(df4["polarity"])
+    # print(df4["10polarity"])
+    # print(df4["roundedPolarity"])
 
-# from sklearn.datasets import make_regression
 
-# X, y = make_regression(n_samples=10, n_features=4, random_state=0)
 
-# print(X)
-# print(y)
+    # from sklearn.datasets import make_regression
 
-X_train, X_test, y_train, y_test = train_test_split(df4['post_text'], df4['10Polarity'], test_size=0.2, random_state=2)
+    # X, y = make_regression(n_samples=10, n_features=4, random_state=0)
 
-# Convert the text data into numerical features using a CountVectorizer
-vectorizer = CountVectorizer()
-X_train = vectorizer.fit_transform(X_train)
-X_test = vectorizer.transform(X_test)
+    # print(X)
+    # print(y)
 
-# print(X_train)
-# print(y_train)
+    X_train, X_test, y_train, y_test = train_test_split(df4['post_text'], df4['10Polarity'], test_size=0.2, random_state=2)
 
-# X_train, X_test, y_train, y_test = train_test_split(df4['roundedPolarity'], df4['severity'], test_size=0.2, random_state=2)
+    # Convert the text data into numerical features using a CountVectorizer
+    vectorizer = CountVectorizer()
+    X_train = vectorizer.fit_transform(X_train)
+    X_test = vectorizer.transform(X_test)
 
-# print(X_train)
+    # print(X_train)
+    # print(y_train)
 
-# print(X_train)
-# print(y_train)
-# X_train = np.reshape(X_train, (-1, 1))
-# X_test = np.reshape(X_test,(-1, 1))
+    # X_train, X_test, y_train, y_test = train_test_split(df4['roundedPolarity'], df4['severity'], test_size=0.2, random_state=2)
 
-# Create a DecisionTreeRegressor
-# regr = DecisionTreeRegressor()  # You can adjust max_depth as needed
-# regr = RandomForestRegressor(max_depth=100)
-regr = DecisionTreeClassifier()
-# regr = RandomForestClassifier()
-# regr = KNeighborsClassifier()
-# regr = MultinomialNB()
-regr.fit(X_train, y_train)
+    # print(X_train)
 
-# Evaluate the classifier on the testing set
-accuracy = regr.score(X_test, y_test)
-print('Accuracy:', accuracy)
+    # print(X_train)
+    # print(y_train)
+    # X_train = np.reshape(X_train, (-1, 1))
+    # X_test = np.reshape(X_test,(-1, 1))
 
-import joblib
+    # Create a DecisionTreeRegressor
+    # regr = DecisionTreeRegressor()  # You can adjust max_depth as needed
+    # regr = RandomForestRegressor(max_depth=100)
+    regr = DecisionTreeClassifier()
+    # regr = RandomForestClassifier()
+    # regr = KNeighborsClassifier()
+    # regr = MultinomialNB()
+    regr.fit(X_train, y_train)
 
-# Save the model to a file
-model_filename = "./Sentiment_Analysis/model/model.pkl"
-joblib.dump(regr, model_filename)
+    # Evaluate the classifier on the testing set
+    accuracy = regr.score(X_test, y_test)
+    print('Accuracy:', accuracy)
 
-# Create a classification report
-# print(classification_report(y_test, regr.predict(X_test)))
+    import joblib
+
+    # Save the model to a file
+    model_filename = "./Sentiment_Analysis/models/model.pkl"
+    joblib.dump(regr, model_filename)
+
+    # Create a classification report
+    # print(classification_report(y_test, regr.predict(X_test)))
+# make_model()
