134adec23904dc14ae7d190ea0f178243e1b77a0
Alan Jiang <alanzjiang10@gmail.com>
2024-03-24 09:10:42 -0400

    Backend updated

--- illuminary/app.py
+++ illuminary/app.py
@@ -1,7 +1,5 @@
-import sqlite3
 from flask import Flask, render_template
 import sys 
-sys.path.append("./illuminary/illuminary")
 sys.path.append("./Sentiment_Analysis")
 
 from api_backend import create_driver, sign_into_facebook, webscrape_facebook_friends, webscrape_friend_posts
@@ -10,27 +8,15 @@ import joblib
 
 app = Flask(__name__)
 
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
+model = joblib.load("./Sentiment_Analysis/models/model.pkl")
 
 @app.route('/')
 def index():
-    return render_template('thing.html')
-    # conn = get_db_connection()
-    # data = conn.execute('SELECT * FROM characters').fetchall()
-    # conn.close()
-    
-    # return render_template('characters.html', characters=data)
+    return render_template('page1.html')
 
 @app.route('/submit?<url>')
 def character(url):
     url = "https://www.facebook.com/profile.php?id=100005539517476"
-    model = joblib.load("./Sentiment_Analysis/model/model.pkl")
-    driver = create_driver()
-    sign_into_facebook(driver)
     friend_list = webscrape_facebook_friends(url)
     worry_list = []
     for friend in friend_list:
